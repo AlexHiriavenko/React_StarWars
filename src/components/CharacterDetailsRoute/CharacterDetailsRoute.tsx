f7081b38c1ef087b4d72c4ea58dbab930c2755cf
Alex <martmarchmartmarch@gmail.com>
2025-08-10 15:23:06 +0300

    implement RTK Query into project

--- src/components/CharacterDetailsRoute/CharacterDetailsRoute.tsx
+++ src/components/CharacterDetailsRoute/CharacterDetailsRoute.tsx
@@ -1,42 +1,23 @@
 import type { Character } from '@/types/AppTypes';
-import { useEffect, useState } from 'react';
 import { useParams, useNavigate, useOutletContext } from 'react-router-dom';
 import { Card } from '@/components/Card';
-import { CharacterService } from '@/services';
+import { useGetPersonQuery } from '@/redux/api/swapiApi';
 
 function CharacterDetailsRoute(): JSX.Element {
   const { id } = useParams<{ id: string }>();
-  const [card, setCard] = useState<Character | null>(null);
-  const [loadingDetails, setLoadingDetails] = useState(false);
   const navigate = useNavigate();
   const { searchParams } = useOutletContext<{
     searchParams: URLSearchParams;
   }>();
 
-  useEffect(() => {
-    if (!id) return;
-
-    const fetchCharacter = async (): Promise<void> => {
-      try {
-        setLoadingDetails(true);
-        const service = new CharacterService();
-        const result = await service.fetchCharacter(id);
-        setCard(result);
-      } catch (error) {
-        console.error('Error fetching character details:', error);
-        setCard(null);
-      } finally {
-        setLoadingDetails(false);
-      }
-    };
-
-    fetchCharacter();
-  }, [id]);
+  const { data, isLoading } = useGetPersonQuery(id as string, {
+    skip: !id,
+  });
 
   return (
     <Card
-      card={card}
-      loadingDetails={loadingDetails}
+      card={(data as Character) ?? null}
+      loadingDetails={isLoading}
       closeCard={() => navigate(`/?${searchParams.toString()}`)}
     />
   );
