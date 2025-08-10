3bc17049b148fda6ab60c998a4cdd4376b4eae40
Alex <martmarchmartmarch@gmail.com>
2025-08-10 21:09:02 +0300

    refactor: remove old services

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,2 @@
-export { BaseService } from './BaseService.ts';
-export { CharacterService } from './CharacterService.ts';
 export { downloadCharactersAsCSV } from './downloadCSV.ts';
 export { DownloadFileService } from './DownloadFileService.ts';
